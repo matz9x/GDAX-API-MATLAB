9d5c7e722a2ad452f34c23abc7a879034df8268d
matz9x <32924751+matz9x@users.noreply.github.com>
2018-01-08 16:44:10 +0100

    Data interpolation + minor bug corrections

--- gdaxfetch.m
+++ gdaxfetch.m
@@ -1,26 +1,47 @@
-function [ frame, dates ] = gdaxfetch(product, start_date, end_date, granularity)
+function [ frame_table ] = gdaxfetch(product, start_date, end_date, granularity)
 %GDAXFETCH
+    start_date_u = posixtime(datetime(start_date));
+    end_date_u = posixtime(datetime(end_date));
 
-time_span = seconds(datetime(end_date) - datetime(start_date));
-num_entries = time_span / granularity;
-max_entries = 350;
-num_calls = ceil(num_entries / max_entries);
-time_steps = (time_span / ceil(num_calls));
+    time = (start_date_u:granularity:end_date_u)';
 
-starting = datetime(start_date);
-frame = nan(1,6);
-while i <= num_calls
-    ending = starting + seconds(time_steps);
-    rolling_frame = flipud(gdaxapi (product, starting, ending, granularity));
-    frame = cat(1, frame, rolling_frame);    
-    starting = ending;
-    i = i + 1;
-    pause(0.5);    
-end
+    time_span = seconds(datetime(end_date) - datetime(start_date));
+    num_entries = time_span / granularity;
+    max_entries = 348;
+    num_calls = ceil(num_entries / max_entries);
+    time_steps = (time_span / ceil(num_calls));
 
-frame(1,:) = [];
-frame(frame(:,1) > posixtime(datetime(end_date)), :) = [];
-dates = datetime(frame(:,1),'ConvertFrom','posixtime');
+    starting = datetime(start_date);
+    frame = nan(1,6);
+
+    i = 1;
+    while i <= num_calls
+        ending = starting + seconds(time_steps);
+        rolling_frame = flipud(gdaxapi (product, starting, ending, granularity));
+        frame = cat(1, frame, rolling_frame);    
+        starting = ending;
+        i = i + 1;
+        pause(0.5);    
+    end
+
+    frame(1,:) = [];
+    frame(frame(:,1) > posixtime(datetime(end_date)), :) = [];
+
+    new_frame = nan(length(time)-1, 6);
+    k = 1;
+    for i = 1:length(time)-1
+        if frame(k,1) == time(i,1)
+            new_frame(i,:) = [time(i,1) frame(k,2:end)];
+            k = k + 1;
+        else
+            new_frame(i) = time(i,1);
+        end
+    end
+
+    dates = datetime(new_frame(:,1),'ConvertFrom','posixtime');
+    frame_table = [array2table(dates) array2table(new_frame, ...
+        'VariableName', {'timestamp', 'low', 'high', 'open', 'close', 'volume'})];
+    frame_table = fillmissing(frame_table, 'spline');
 
 %% Functions
 function [ dataframe ] = gdaxapi (product, start_date, end_date, granularity)
@@ -34,4 +55,3 @@ function [ dataframe ] = gdaxapi (product, start_date, end_date, granularity)
     dataframe = webread(website);
 end
 end
-
